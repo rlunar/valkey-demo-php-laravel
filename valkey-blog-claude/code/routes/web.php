d6a303f495b83d26e5d8b71b355126c949d4805c
Roberto Luna Rojas <rberoj+sns@amazon.com>
2025-09-23 15:40:08 -0300

    Valkey demo PHP Laravel: Cleanup

--- valkey-blog-claude/code/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BlogController;
-
-Route::get('/', [BlogController::class, 'index'])->name('blog.index');
-Route::get('/post/{post}', [BlogController::class, 'show'])->name('blog.show');
-Route::get('/category/{category}', [BlogController::class, 'category'])->name('blog.category');
-Route::post('/post/{post}/comment', [BlogController::class, 'storeComment'])->name('blog.comment');
